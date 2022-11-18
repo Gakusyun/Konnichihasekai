114bd8f3f2cc996ca0a4ea0bdd748420da1970bd
Gakusyun <67746154+Gakusyun@users.noreply.github.com>
2022-11-18 20:01:17 +0800

    Update 3.kt

--- 3.kt
+++ 3.kt
@@ -1,3 +1,12 @@
-fun main(){
-	println("hello World!")
+fun main() {
+	sec()
+	println(sec())
+}
+fun sec(): String {
+	thi()
+	return(thi())
+}
+fun thi(): String {
+	val fou: String = "Hello World!"
+	return(fou)
 }
